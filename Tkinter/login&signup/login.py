5eb2433500ccfcea25379aad028ab5aa0c4320a2
Chittoji Murali Sree Krishna <cmkrishna97@gmail.com>
2020-08-31 11:01:57 +0530

    Update login.py

--- Tkinter/login&signup/login.py
+++ Tkinter/login&signup/login.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
+from PIL import ImageTk, Image
 
 root = tk.Tk()
 root.configure(bg='black')
@@ -33,6 +34,7 @@ eml = Entry(root, bg='grey', fg='black')
 passw = Entry(root, bg='grey', fg='black')
 login = Button(root, bg='dodger blue', text='Login',
                fg='white', font=('bold'), command=login)
+my_img = ImageTk.PhotoImage(Image.open("/home/wargun/Pictures/mylogo.png"))
 
 
 # labels for the entries
@@ -40,8 +42,9 @@ email_l = Label(root, text='Enter Your Email :', bg='black',
                 fg='white', font=('monospace', 16, 'bold'))
 passw_l = Label(root, text='Enter Your Password :', bg='black',
                 fg='white', font=('monospace', 16, 'bold'))
-
-
+my_label = Label(image=my_img, bg='black')
+my_label2 = Label(root, text='Login Form', bg='black',
+                  fg='white', font=('monospace', 30, 'bold'))
 # setiinng up place for the entries
 eml.grid(row=1, column=1)
 passw.grid(row=2, column=1)
@@ -49,6 +52,8 @@ login.grid(row=3, column=1)
 
 
 # setting up place for the labels
+my_label.grid(row=0, column=0)
+my_label2.grid(row=0, column=1)
 email_l.grid(row=1, column=0)
 passw_l.grid(row=2, column=0)
 
