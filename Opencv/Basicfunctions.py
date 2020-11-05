868aab60eb8624e97eb84fdabee7583e354b5464
Chittoji Murali Sree Krishna <cmkrishna97@gmail.com>
2020-11-05 20:54:00 +0530

    Update Basicfunctions.py

--- Opencv/Basicfunctions.py
+++ Opencv/Basicfunctions.py
@@ -12,6 +12,10 @@ imEroded = cv2.erode(imDialation, kernel, iterations=1)
 imResize = cv2.resize(img,(250,250))
 imCropped = img[0:200,200:500]
 
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
+cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.putText(img,"Hey Warlord",(300,150),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
 
 cv2.imshow('Image', img)
 cv2.imshow('Gray image', imGrey)
