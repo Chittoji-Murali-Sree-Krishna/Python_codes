d0ed6ca276416083dfa9d351efb4fe072b871c1c
Chittoji Murali Sree Krishna <cmkrishna97@gmail.com>
2020-11-06 16:41:28 +0530

    Update faceRecog.py

--- Opencv/facerecognition/faceRecog.py
+++ Opencv/facerecognition/faceRecog.py
@@ -1,47 +1,46 @@
-import os
-import cv2
-from PIL import Image
 import numpy as np
+import cv2
 import pickle
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "images")
-
+#! give the full path to avoid errors
 face_cascade = cv2.CascadeClassifier('/home/wargun/Documents/python/opencv/facerecog/cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('/home/wargun/Documents/trainner.yml')
+
+labels = {"person_name": 1}
+with open('labels.pickle', 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
 
 
-current_id = 0
-label_ids = {}
-y_labels = []
-x_train = []
+while(True):
+    ret, frame = cap.read()
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=4 and conf <= 85:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2 
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+        color = (255, 0, 0)
+        stroke = 2
+        endCord_x = x + w
+        endCord_y = y + h
+        cv2.rectangle(frame, (x, y), (endCord_x, endCord_y), color, stroke)
+        
 
-for root, dirs, files in os.walk(image_dir):
-	for file in files:
-		if file.endswith("png") or file.endswith("jpg"):
-			path = os.path.join(root, file)
-			label = os.path.basename(root).replace(" ", "-").lower()
-			if not label in label_ids:
-				label_ids[label] = current_id
-				current_id += 1
-			id_ = label_ids[label]
-			pil_image = Image.open(path).convert("L")
-			size = (550, 550)
-			final_image = pil_image.resize(size, Image.ANTIALIAS)
-			image_array = np.array(final_image, "uint8")
-			#print(image_array)
-			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) == ord('q'):
+        break
 
-			for (x,y,w,h) in faces:
-				roi = image_array[y:y+h, x:x+w]
-				x_train.append(roi)
-				y_labels.append(id_)
-
-#print(x_train)
-#print(y_labels)
-
-with open("labels.pickle", 'wb') as f:
-	pickle.dump(label_ids, f)
-
-recognizer.train(x_train, np.array(y_labels))
-recognizer.save("trainner.yml")
+cap.release()
+cv2.destroyAllWindows()
